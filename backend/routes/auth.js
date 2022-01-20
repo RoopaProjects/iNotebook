204d31f59997e5d70fa7894435ae23526d31f9e5
roop <roopzcs36@gmail.com>
2022-01-20 14:41:50 +0530

    Storing data into database using mongoose model

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const User=require("../models/User");
 
-router.get("/", (req, res) => {
- const  obj = { a: "thios", number: 34 }
-  res.json(obj);
+router.post("/", (req, res) => {
+ console.log(req.body);
+ const user=User(req.body);
+ user.save()
+ res.send(req.body);
 });
 module.exports = router;
